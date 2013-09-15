2aa471922d6eb9fc815c2790730c75e0dc7dbf38
Andre Le <andre@andrele.com>
2013-09-15 00:46:14 -0400

    Updated BrowseMode typography

--- js/layoutengine.js
+++ js/layoutengine.js
@@ -2,14 +2,12 @@ function onFullMode(){
 	document.getElementById('body').className = "full";
 	$(".heading").removeClass("col-md-4");
 	$(".video").removeClass("col-md-8");
-	$(".shortText").removeClass("col-md-12");
 }
 
 function onBrowseMode(){
 	document.getElementById('body').className = "browse";
 	$(".heading").addClass("col-md-4");
 	$(".video").addClass("col-md-8");
-	$(".shortText").addClass("col-md-4");
 }
 
 function onDetailMode(){
